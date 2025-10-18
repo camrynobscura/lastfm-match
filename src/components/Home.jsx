edec170883b6adb4eed32292e8a4bd268f8f731d
Camryn Pearson <camrynpearson@gmail.com>
2025-10-17 22:46:45 -0400

    added data fetching from lastfm api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { searchUsername } from '../services/api'
 
 const Home = () => {
   // data from form input
   let [usernameOne, setUsernameOne] = useState('')
   let [usernameTwo, setUsernameTwo] = useState('')
+  let [timePeriod, setTimePeriod] = useState('1month')
 
   // data from api
   let [usernameOneData, setusernameOneData] = useState()
@@ -13,26 +15,31 @@ const Home = () => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
-  async function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    console.log('hello')
+    // console.log(timePeriod)
 
     // check if fields are empty
-    if (!usernameOne.trim()) return
-    if (!usernameTwo.trim()) return
+    // if (!usernameOne.trim()) return
+    // if (!usernameTwo.trim()) return
 
-    if (isLoading) return
-    setIsLoading(true)
+
+
+    // if (isLoading) return
+    // setIsLoading(true)
     try {
-
+      console.log('hello')
       // make api request with two usernames
-      let usernameOneResults = await searchUsername(usernameOne)
-      let usernameTwoResults = await searchUsername(usernameTwo)
+      let usernameOneResults = await searchUsername(usernameOne, timePeriod)
+      let usernameTwoResults = await searchUsername(usernameTwo, timePeriod)
 
       // save the results
       setusernameOneData(usernameOneResults)
       setusernameTwoData(usernameTwoResults)
 
       // compare the two data objects
+      compareUsers(usernameOneData, usernameTwoData)
 
       setError(null)
     } catch (err) {
@@ -41,6 +48,11 @@ const Home = () => {
     } finally {
       setIsLoading(false)
     }
+  }
+
+  function compareUsers(userOne, userTwo) {
+    console.log(userOne)
+    console.log(userTwo)
   }
 
   return (
@@ -69,6 +81,14 @@ const Home = () => {
             value={usernameTwo}
             onChange={(e) => setUsernameTwo(e.target.value)}
           />
+          <select name='' id='' onChange={(e) => setTimePeriod(e.target.value)}>
+            <option defaultValue value='7day'>1 Week</option>
+            <option value='1month'>1 Month</option>
+            <option value='3month'>3 Months</option>
+            <option value='6month'>6 Months</option>
+            <option value='12month'>1 Year</option>
+            <option value='overall'>All Time</option>
+          </select>
           <button type='submit'>Match</button>
         </form>
       </div>
